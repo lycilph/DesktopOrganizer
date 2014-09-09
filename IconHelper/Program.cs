3c6ad865f3c2237cad04397193365d031d9de747
lycilph <lycilph@gmail.com>
2014-09-09 10:10:06 +0200

    Added logging to IconHelper project

--- IconHelper/Program.cs
+++ IconHelper/Program.cs
@@ -1,46 +1,59 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Pipes;
 using System.Windows;
 using Core;
 using Core.Data;
 using Newtonsoft.Json;
+using NLog;
 
 namespace IconHelper
 {
     class Program
     {
+        protected static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main()
         {
-            using (var client = new NamedPipeClientStream(".", "DesktopOrganizerPipe", PipeDirection.InOut))
+            try
             {
-                client.Connect();
-
-                var sw = new StreamWriter(client);
-                var sr = new StreamReader(client);
-
-                var input = sr.ReadLine();
-                switch (input)
+                using (var client = new NamedPipeClientStream(".", "DesktopOrganizerPipe", PipeDirection.InOut))
                 {
-                    case "get":
+                    client.Connect();
+
+                    var sw = new StreamWriter(client);
+                    var sr = new StreamReader(client);
+
+                    var input = sr.ReadLine();
+                    switch (input)
+                    {
+                        case "get":
                         {
+                            logger.Trace("IconHelper - getting icon positions");
                             var icons = IconManager.GetIconsPositions();
                             var output = JsonConvert.SerializeObject(icons);
                             sw.WriteLine(output);
                             sw.Flush();
                             client.WaitForPipeDrain();
                         }
-                        break;
-                    case "set":
+                            break;
+                        case "set":
                         {
+                            logger.Trace("IconHelper - applying layout");
                             var json = sr.ReadLine();
                             var layout = JsonConvert.DeserializeObject<Layout<Icon>>(json);
                             IconManager.ApplyLayout(layout);
                         }
-                        break;
-                    default:
-                        MessageBox.Show(string.Format("Unknown command [{0}]", input));
-                        break;
+                            break;
+                        default:
+                            MessageBox.Show(string.Format("Unknown command [{0}]", input));
+                            break;
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.Message);
             }
         }
     }
